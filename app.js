f3e76a8bc9ea11c20ebe7ecde137eba5a4b5b859
mdforhad80 <mdforhad@kakao.com>
2026-05-19 11:07:13 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const animeGrid = document.getElementById('animeGrid');
+const searchInput = document.getElementById('searchInput');
+
+async function fetchTrendingAnime() {
+
+  try {
+
+    animeGrid.innerHTML = "<p>Loading anime...</p>";
+
+    const response = await fetch(
+      'https://api.jikan.moe/v4/top/anime'
+    );
+
+    const result = await response.json();
+
+    displayAnime(result.data);
+
+  } catch (error) {
+
+    console.log(error);
+
+    animeGrid.innerHTML =
+      "<p>Failed to load anime.</p>";
+  }
 }
 
 function displayAnime(animes) {
 
   animeGrid.innerHTML = '';
 
-  animes.forEach(anime => {
+  animes.slice(0, 12).forEach(anime => {
 
     const card = document.createElement('div');
 
     card.classList.add('card');
 
     card.innerHTML = `
-      <img src="${anime.images.jpg.large_image_url}">
+      <img src="${anime.images.jpg.large_image_url}" />
 
       <div class="card-content">
+
         <h3>${anime.title}</h3>
 
         <div class="meta">
-          Score: ${anime.score || 'N/A'}
+          ⭐ ${anime.score || 'N/A'}
         </div>
+
       </div>
     `;
 
@@ -43,14 +68,20 @@ searchInput.addEventListener('keyup', async () => {
     return;
   }
 
-  const response = await fetch(
-    `https://api.jikan.moe/v4/anime?q=${query}`
-  );
+  try {
 
-  const data = await response.json();
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${query}`
+    );
 
-  displayAnime(data.data);
+    const result = await response.json();
 
+    displayAnime(result.data);
+
+  } catch(error) {
+
+    console.log(error);
+  }
 });
 
 fetchTrendingAnime();
